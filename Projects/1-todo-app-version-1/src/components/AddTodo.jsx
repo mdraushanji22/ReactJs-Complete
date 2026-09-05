d63616f28840539cc6bf05320d212ff9644ae55a
mdraushan <mdraushanji22@gmail.com>
2026-09-05 18:56:36 +0530

    ReactJS-Complete

---
+++ Projects/1-todo-app-version-1/src/components/AddTodo.jsx
@@ -0,0 +1,23 @@
+function AddTodo() {
+  return (
+    <div>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter Todo Here" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              ADD
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
---
+++ Projects/1-todo-app-version-1/src/components/TodoItems.jsx
@@ -0,0 +1,19 @@
+function TodoItems() {
+  return (
+    <div>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">Buy Milk</div>
+          <div class="col-4">05/10/2026</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItems;
